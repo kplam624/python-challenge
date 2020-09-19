be176f937881431fe4671008c998e8155bc45ad6
kplam624 <kplam624@gmail.com>
2020-09-19 15:47:12 -0400

    Cleaned up code

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,8 @@ total = 0
 #Creates a new list to determine the total profit
 total_pl = []
 
+print("Financial Analysis")
+print("-------------------------")
 #Open CSV and read
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -14,24 +16,19 @@ with open(csvpath) as csvfile:
     for rows in csvreader:
 ## To see the set        print(rows[1])
         months += 1
-        
+print("Total Months:", months)        
+
 #Appends the profit/loss values to the new list
         total_pl.append(rows[1])
-
 #Find the total amount of profit and losses
 #Removes the header and adds up the changes
     total_pl.pop(0)
     for change in total_pl:
         total += int(change)
-
+print(f"Total: ${total}")
 
 print("Hi.")
-print(total_pl)
-print(total)
-print("Financial Analysis")
-print("-------------------------")
-print("Total Months:", months)
-print(f"Total: ${total}")
+
 print("Average Change: ")
 print("Greatest Increase in Profits: ")
 print("Greatest Decrease in Profits: ")
