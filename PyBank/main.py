65f07d67d481e4953240739e2561c700cdc85208
kplam624 <kplam624@gmail.com>
2020-09-22 18:22:14 -0400

    Added code to find largest change

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,13 @@ import csv
 csvpath = os.path.join('..','Resources','budget_data.csv')
 months = -1
 total = 0
+big = 0
+small = 0
 #Creates a new list to determine the total profit
 total_pl = []
+
+#Creates a new list for both months and profits
+big_l = []
 
 #Header of the Financial Analysis
 print("Financial Analysis")
@@ -20,6 +25,8 @@ with open(csvpath) as csvfile:
         months += 1     
 #Appends the profit/loss values to the new list
         total_pl.append(rows[1])
+#Also appends the month to the new list
+        prof_mon.append(rows)
 #Find the total amount of profit and losses
 #Removes the header and adds up the changes
     total_pl.pop(0)
@@ -31,11 +38,18 @@ with open(csvpath) as csvfile:
 #Difference of the last month value and first month value divided by the total months
     avg_change = (int(total_pl[last]) - int(total_pl[0]))/months
 
+##Attempt to find the greatest changes
+    big_l.append(total_pl[0])    
+    for i in range(len(total_pl)-1):
+        big_l = [total_pl[i+1] - total_pl[i]]
+
+
+#prints the rest of the stuff
 print("Total Months:", months)   
 print(f"Total: ${total}")
 print(f"Average Change: ${avg_change:.2f}")
 #print("Greatest Increase in Profits: ")
 #print("Greatest Decrease in Profits: ")
 
-##print(total_pl)
+print(big)
 ##print("Hi.")
