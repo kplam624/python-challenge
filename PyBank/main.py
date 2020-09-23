7e3b7360b454b8e939aa6e8b6cc325ea4d3b76ee
kplam624 <kplam624@gmail.com>
2020-09-22 22:49:49 -0400

    Added the ability to write a .txt file. Also added a .txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,7 +17,7 @@ prof_mon = []
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+
 #Counts the months in the csv
     for rows in csvreader:
         months += 1     
@@ -26,15 +26,13 @@ with open(csvpath) as csvfile:
 #Also appends the month to the new list
         prof_mon.append(rows[0])
 #Find the total amount of profit and losses
-#Removes the header and adds up the changes
-#    total_pl.pop(0)
     for change in total_pl:
         total += int(change)
 #Finds the last value of the list    
     last = len(total_pl)-1
 
 #Average change Calculation
-#Difference of the last month value and first month value divided by the changes(month -1)
+#Difference of the last month value and first month value divided by the changes(month - 1)
     avg_change = (int(total_pl[last]) - int(total_pl[0]))/(months-1)
 
 #Find the changes and inserts them into a new list
@@ -42,7 +40,7 @@ with open(csvpath) as csvfile:
     big = 0    
     for i in range(len(total_pl)-1):
         big_l.append(int(total_pl[i+1]) - int(total_pl[i]))
-    
+#Does a comparison to find the greatest increase and greatest decrease    
     for i in range(len(total_pl)):
         num = big_l[i]
         if int(num) > big:
@@ -54,12 +52,24 @@ with open(csvpath) as csvfile:
         else:
             continue
 
+#Creates a .txt file
+mainfile = open("main.txt","w")
+
+mainfile.write("Financial Analysis\n")
+mainfile.write("-------------------------\n")
+mainfile.write(f"Total Months: {months}\n")
+mainfile.write(f"Total: ${total}\n")
+mainfile.write(f"Average Change: ${avg_change:.2f}\n")
+mainfile.write(f"Greatest Increase in Profits: {prof_mon[inc]} (${big})\n")
+mainfile.write(f"Greatest Decrease in Profits:  {prof_mon[dec]} (${small})\n")
+mainfile.close()
+
 #Header of the Financial Analysis
 print("Financial Analysis")
-print("-------------------------")
+print("---------------------------")
 
 #prints the rest of the stuff
-print("Total Months:", months)   
+print(f"Total Months: {months}")   
 print(f"Total: ${total}")
 print(f"Average Change: ${avg_change:.2f}")
 print(f"Greatest Increase in Profits: {prof_mon[inc]} (${big})")
