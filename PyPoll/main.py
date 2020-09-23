ea0564eb7ee649806e636dbfec4e7f0f2ae42db2
kplam624 <kplam624@gmail.com>
2020-09-23 12:18:25 -0400

    Added a dictionary and a list holding candidate names

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,8 +2,8 @@ import os
 import csv
 
 count = 0
-
-
+candidate = []
+cand_dic = {}
 csvpath = os.path.join('..','Resources','election_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -11,11 +11,15 @@ with open(csvpath) as csvfile:
 
     for rows in csvreader:
         count += 1
+        if rows[2] not in candidate:
+            candidate.append(rows[2])
+        else:
+            continue
+
+cand_dic["Candidate Name"] = candidate
 
 
 
 
-
-
-
-##print(count)
+print(cand_dic)
+print(count)
