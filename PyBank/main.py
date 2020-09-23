164155d9cd51280048bb43754609a1f6c7dfdfed
kplam624 <kplam624@gmail.com>
2020-09-22 20:45:36 -0400

    Added the greatest decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,12 +22,11 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 #Counts the months in the csv
     for rows in csvreader:
-## To see the set        print(rows[1])
         months += 1     
 #Appends the profit/loss values to the new list
         total_pl.append(rows[1])
 #Also appends the month to the new list
-        prof_mon.append(rows)
+        prof_mon.append(rows[0])
 #Find the total amount of profit and losses
 #Removes the header and adds up the changes
     total_pl.pop(0)
@@ -46,11 +45,14 @@ with open(csvpath) as csvfile:
     for i in range(len(total_pl)-1):
         big_l.append(int(total_pl[i+1]) - int(total_pl[i]))
     
-    for i in range(len(total_pl)-1):
+    for i in range(len(total_pl)):
         num = big_l[i]
         if int(num) > big:
             big = int(num)
-            inc = i
+            inc = i+1
+        elif int(num) < small:
+            small = int(num)
+            dec = i+1
         else:
             continue
 
@@ -59,8 +61,5 @@ with open(csvpath) as csvfile:
 print("Total Months:", months)   
 print(f"Total: ${total}")
 print(f"Average Change: ${avg_change:.2f}")
-#print("Greatest Increase in Profits: ", prof_mon[inc] )
-#print("Greatest Decrease in Profits: ")
-
-print(big)
-##print("Hi.")
+print(f"Greatest Increase in Profits: {prof_mon[inc]} (${big})")
+print(f"Greatest Decrease in Profits:  {prof_mon[dec]} (${small})")
